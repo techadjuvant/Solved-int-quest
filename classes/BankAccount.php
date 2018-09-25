050e892602e04f1b43192622603b8dc833917690
Motahar Hossain <motahar1201123@gmail.com>
2018-09-25 16:21:44 +0600

    Fatal error solved

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -20,6 +20,12 @@ class BankAccount implements IfaceBankAccount
         //implement this method
     }
 
+    public function withdraw(Money $amount)
+    {
+        
+    }
+
+
     public function transfer(Money $amount, BankAccount $account)
     {
         //implement this method
